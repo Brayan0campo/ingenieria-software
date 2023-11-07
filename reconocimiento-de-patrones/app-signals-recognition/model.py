2b513e4c90b97ed460c9130f3da3d6a8431486f9
Brayan <ocamporodriguezbrayan@gmail.com>
2023-11-07 14:33:13 -0500

    Configuration model features

--- reconocimiento-de-patrones/app-signals-recognition/model.py
+++ reconocimiento-de-patrones/app-signals-recognition/model.py
@@ -1,4 +1,5 @@
 import os
+import joblib
 import librosa
 import numpy as np
 import pandas as pd
@@ -14,117 +15,168 @@ Muestreo = 250000
 Dataframe = np.zeros((Total, Muestreo + 1))
 
 # Leer las etiquetas
-Labels = pd.read_excel('labels/Labels.xlsx', header=None)
-Y = Labels.iloc[:, 0].values.astype(int)
+try:
+    Labels = pd.read_excel('labels/Labels.xlsx', header=None)
+    Y = Labels.iloc[:, 0].values.astype(int)
+except Exception as e:
+    print(f"Error al cargar etiquetas: {e}")
+    exit()
 
+# Extraer características
 X = []
 for i in range(1, Total + 1):
     for ext in ['mp3', 'aac']:
         Ruta_audios = os.path.join(Ruta, str(i) + '.' + ext)
         if os.path.exists(Ruta_audios):
-            Señal, sr = librosa.load(Ruta_audios)
-            Señal_New = np.resize(Señal, Muestreo)
+            try:
+                Señal, sr = librosa.load(Ruta_audios)
+                Señal_New = np.resize(Señal, Muestreo)
 
-            # Extraer características
-            Magnitud, _ = librosa.magphase(librosa.stft(Señal))
+                Magnitud, _ = librosa.magphase(librosa.stft(Señal))
+                RMS_vector = librosa.feature.rms(S=Magnitud)
+                RMS = RMS_vector.mean()
+                Times_vector = librosa.times_like(RMS_vector)
+                Times = Times_vector.mean()
+                Varianza = np.var(Señal)
+                rms_amplitude = np.sqrt(np.mean(np.square(Señal)))
+                Desviacion = np.std(Señal)
+                zero_crossings = np.where(np.diff(np.sign(Señal)))[0]
+                zcr = len(zero_crossings)
+                skewness = np.abs(
+                    np.mean((Señal - np.mean(Señal)) ** 3) / (np.std(Señal) ** 3))
 
-            RMS_vector = librosa.feature.rms(S=Magnitud)
-            RMS = RMS_vector.mean()
-
-            Times_vector = librosa.times_like(RMS_vector)
-            Times = Times_vector.mean()
-
-            Varianza = np.var(Señal)
-
-            rms_amplitude = np.sqrt(np.mean(np.square(Señal)))
-
-            Desviacion = np.std(Señal)
-
-            zero_crossings = np.where(np.diff(np.sign(Señal)))[0]
-            zcr = len(zero_crossings)
-
-            skewness = np.abs(np.mean((Señal - np.mean(Señal)) ** 3) / (np.std(Señal) ** 3))
-
-            """ fft = np.fft.fft(Señal_New)
-            frecuencias = np.fft.fftfreq(len(fft)) """
-
-            # Almacenar vector de características
-            caracteristicas = [RMS, Times, Varianza, rms_amplitude, Desviacion, zcr, skewness]
-            Dataframe[i - 1, 0:Muestreo] = np.transpose(Señal_New)
-            Dataframe[i - 1, Muestreo] = Y[i - 1]
-            """ Dataframe[i - 1, Muestreo + 1] = max(frecuencias) """
-            X.append(caracteristicas)
-            break
+                """
+                fft = np.fft.fft(Señal_New)
+                frecuencias = np.fft.fftfreq(len(fft))
+                """
+                caracteristicas = [RMS, Times, Varianza,
+                                   rms_amplitude, Desviacion, zcr, skewness]
+                Dataframe[i - 1, 0:Muestreo] = np.transpose(Señal_New)
+                Dataframe[i - 1, Muestreo] = Y[i - 1]
+                """ Dataframe[i - 1, Muestreo + 1] = max(frecuencias) """
+                X.append(caracteristicas)
+                break
+            except Exception as e:
+                print(f"Error en la extracción de características: {e}")
+                continue
+        else:
+            print(f"El archivo de audio {Ruta_audios} no existe.")
 
 # Normalizar características
-scaler = MinMaxScaler()
-X = scaler.fit_transform(X)
+try:
+    scaler = MinMaxScaler()
+    X = scaler.fit_transform(X)
+except Exception as e:
+    print(f"Error al normalizar características: {e}")
 
 # Dividir el conjunto de datos en entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+try:
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+except Exception as e:
+    print(f"Error al dividir el conjunto de datos: {e}")
+    X_train, X_test, y_train, y_test = [], [], [], []
 
 # Entrenar un clasificador KNN
-knn = KNeighborsClassifier(n_neighbors=5)
-knn.fit(X_train, y_train)
+try:
+    model = KNeighborsClassifier(n_neighbors=5)
+    model.fit(X_train, y_train)
+except Exception as e:
+    print(f"Error al entrenar el modelo: {e}")
 
 # Realizar predicciones en el conjunto de prueba
-y_pred = knn.predict(X_test)
+try:
+    y_pred = model.predict(X_test)
+except Exception as e:
+    print(f"Error al realizar predicciones: {e}")
 
 # Calcular la precisión del modelo
-accuracy = accuracy_score(y_test, y_pred)
-print(" ")
-print("Precisión del modelo KNN:", accuracy)
-print("-----------------------------------------------------")
+try:
+    accuracy = accuracy_score(y_test, y_pred)
+    print(" ")
+    print("Precisión del modelo KNN:", accuracy)
+    print("-----------------------------------------------------")
+except Exception as e:
+    print(f"Error al calcular la precisión del modelo: {e}")
 
 # Mostrar reporte de clasificación con precision, recall y F1-score
-report = classification_report(y_test, y_pred)
-print(" ")
-print("Reporte de Clasificación:")
-print(report)
-print("-----------------------------------------------------")
+try:
+    report = classification_report(y_test, y_pred)
+    print(" ")
+    print("Reporte de Clasificación:")
+    print(report)
+    print("-----------------------------------------------------")
+except Exception as e:
+    print(f"Error al mostrar el reporte de clasificación: {e}")
 
 # Calcular la matriz de confusión
-confusion = confusion_matrix(y_test, y_pred)
+try:
+    confusion = confusion_matrix(y_test, y_pred)
+    print(" ")
+    print("Matriz de Confusión:")
+    print(confusion)
+    print(" ")
+    print("-----------------------------------------------------")
+except Exception as e:
+    print(f"Error al calcular la matriz de confusión: {e}")
+
+# Guardado del modelo
+try:
+    joblib.dump(model, 'signals_model.pkl')
+except Exception as e:
+    print(f"Error al guardar el modelo: {e}")
+
+# ---------------------------------------------------------------------------------------------- #
+
+# Carga la nueva señal de audio
+nueva_senal, sr = librosa.load("dataset/96.mp3")
+
+# Extrae características de la nueva señal
+Magnitud, _ = librosa.magphase(librosa.stft(nueva_senal))
+RMS_vector = librosa.feature.rms(S=Magnitud)
+RMS = RMS_vector.mean()
+Times_vector = librosa.times_like(RMS_vector)
+Times = Times_vector.mean()
+Varianza = np.var(nueva_senal)
+rms_amplitude = np.sqrt(np.mean(np.square(nueva_senal)))
+Desviacion = np.std(nueva_senal)
+zero_crossings = np.where(np.diff(np.sign(nueva_senal)))[0]
+zcr = len(zero_crossings)
+skewness = np.abs(np.mean((nueva_senal - np.mean(nueva_senal)) ** 3) / (np.std(nueva_senal) ** 3))
+
+# Almacena las características en una lista
+caracteristicas_nueva_senal = [RMS, Times, Varianza, rms_amplitude, Desviacion, zcr, skewness]
+
+# Normaliza las características
+caracteristicas_nueva_senal_normalizadas = scaler.transform([caracteristicas_nueva_senal])
+
+# Realiza una predicción con el modelo
+prediccion = model.predict(caracteristicas_nueva_senal_normalizadas)
+
+# Muestra la etiqueta predicha
 print(" ")
-print("Matriz de Confusión:")
-print(confusion)
+print("Etiqueta predicha para la nueva señal:", prediccion)
 
+""""
+Precisión del modelo KNN: 1.0
+-----------------------------------------------------
 
+Reporte de Clasificación:
+              precision    recall  f1-score   support
 
-# Ruta del nuevo audio que deseas predecir
-nuevo_audio_path = "dataset/80.mp3"  # Reemplaza con la ruta del nuevo audio
+           0       1.00      1.00      1.00        20
+           1       1.00      1.00      1.00        14
 
-if os.path.exists(nuevo_audio_path):
-    # Cargar el nuevo audio
-    Señal_nuevo, sr_nuevo = librosa.load(nuevo_audio_path)
-    Señal_nuevo = np.resize(Señal_nuevo, Muestreo)  # Redimensionar a la longitud de Muestreo
+    accuracy                           1.00        34
+   macro avg       1.00      1.00      1.00        34
+weighted avg       1.00      1.00      1.00        34
 
-    # Extraer características del nuevo audio
-    Magnitud_nuevo, _ = librosa.magphase(librosa.stft(Señal_nuevo))
-    RMS_vector_nuevo = librosa.feature.rms(S=Magnitud_nuevo)
-    RMS_nuevo = RMS_vector_nuevo.mean()
-    Times_vector_nuevo = librosa.times_like(RMS_vector_nuevo)
-    Times_nuevo = Times_vector_nuevo.mean()
-    Varianza_nuevo = np.var(Señal_nuevo)
-    rms_amplitude_nuevo = np.sqrt(np.mean(np.square(Señal_nuevo)))
-    Desviacion_nuevo = np.std(Señal_nuevo)
-    zero_crossings_nuevo = np.where(np.diff(np.sign(Señal_nuevo)))[0]
-    zcr_nuevo = len(zero_crossings_nuevo)
-    skewness_nuevo = np.abs(np.mean((Señal_nuevo - np.mean(Señal_nuevo)) ** 3) / (np.std(Señal_nuevo) ** 3))
+-----------------------------------------------------
 
-    # Normalizar las características del nuevo audio
-    caracteristicas_nuevo = [RMS_nuevo, Times_nuevo, Varianza_nuevo, rms_amplitude_nuevo, Desviacion_nuevo, zcr_nuevo, skewness_nuevo]
-    caracteristicas_nuevo = scaler.transform([caracteristicas_nuevo])
+Matriz de Confusión:
+[[20  0]
+ [ 0 14]]
 
-    # Realizar la predicción en el nuevo audio
-    prediccion = knn.predict(caracteristicas_nuevo)
+-----------------------------------------------------
 
-    # Imprimir el resultado
-    if prediccion[0] == 0:
-        print(" ")
-        print("El coche está malo (predicción: 0)")
-    else:
-        print(" ")
-        print("El coche está bueno (predicción: 1)")
-else:
-    print("El archivo de audio no se encuentra en la ruta especificada.")
+Etiqueta predicha para la nueva señal: [0]
+"""
